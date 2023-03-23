9ad0d955f879ad839dfb7899e8421accca7c1967
rauda08 <raudajnnh08@gmail.com>
2023-03-23 16:22:25 +0700

    menggunakan perulangan for utk menyimpan data pd arraymasukkan pesan

--- InsertionSort_113/InsertionSort_113.cpp
+++ InsertionSort_113/InsertionSort_113.cpp
@@ -5,6 +5,30 @@ int arr[20];           // Membuat array dengan panjang data 20
 int n; // Membuat variabel inputan n 
 
 void input()
+{  // Procedure Input 
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada array : ";    // Membuat inpitan Jumlah element Array 
+        cin >> n;
+
+        if (n <= 20)
+        {    // Membuat kondisi n tidak lebih dari 20
+            break;
+
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+
+        }
+    }
+    cout << endl; 
+    cout << "======================" << endl; 
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl; 
+
+        // Menggunakan perulangan for untuk menyimpan data pada array  
+
 
 int main()
 {
